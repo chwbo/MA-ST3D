06f83d09c4544f4b696ff7670db28637fc7d20a6
brave-ws-01 <wenbo.chen@ia.ac.cn>
2024-10-15 09:45:32 +0800

    Initial commit

--- pre_nuscenes_odom_dataset.py
+++
@@ -1,2 +0,0 @@
-import runpy
-runpy.run_module('pcdet.datasets.nuscenes_odom.nuscenes_odom_dataset_slam', run_name='__main__')
--- pre_waymo_dataset.py
+++
@@ -1,2 +0,0 @@
-import runpy
-runpy.run_module('pcdet.datasets.waymo.waymo_dataset', run_name='__main__')
